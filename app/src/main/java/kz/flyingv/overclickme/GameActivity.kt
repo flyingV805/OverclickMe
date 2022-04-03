573140f512ae70131b0c1b6d7018ef35c1564527
FlyingV <v.petrov@indev.expert>
2022-04-03 18:57:06 +0600

    Base gameplay working now

--- app/src/main/java/kz/flyingv/overclickme/GameActivity.kt
+++ app/src/main/java/kz/flyingv/overclickme/GameActivity.kt
@@ -1,9 +1,15 @@
 package kz.flyingv.overclickme
 
 import android.animation.LayoutTransition
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.widget.AppCompatButton
 import androidx.constraintlayout.widget.Guideline
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kz.flyingv.overclickme.databinding.ActivityMainBinding
 
 class GameActivity : AppCompatActivity() {
@@ -19,16 +25,57 @@ class GameActivity : AppCompatActivity() {
 
         binding.player0.setOnClickListener {
             battleState += 0.02f
-            binding.battleLine.setGuidelinePercent(battleState)
         }
 
         binding.player1.setOnClickListener {
             battleState -= 0.02f
-            binding.battleLine.setGuidelinePercent(battleState)
         }
 
-        binding.rootLayout.layoutTransition.enableTransitionType(LayoutTransition.CHANGING);
+        binding.rootLayout.layoutTransition.enableTransitionType(LayoutTransition.CHANGING)
 
+        startBattle()
+    }
+
+    private fun showWinDialog(){
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_win_dialog)
+        dialog.setCancelable(false)
+
+        dialog.findViewById<AppCompatButton>(R.id.again).setOnClickListener {
+            dialog.dismiss()
+            restartBattle()
+        }
+
+        dialog.show()
+    }
+
+    private fun finishBattle(){
+        if(battleState <= 0.1){
+            showWinDialog()
+        }else if(battleState >= 0.9){
+            showWinDialog()
+        }
+    }
+
+    private fun restartBattle(){
+        battleState = 0.5f
+        binding.battleLine.setGuidelinePercent(battleState)
+        startBattle()
+    }
+
+    private fun startBattle(){
+        var isPlaying = true
+        lifecycleScope.launch {
+            while (isPlaying){
+                binding.battleLine.setGuidelinePercent(battleState)
+                delay(33)
+                if(battleState <= 0.1 || battleState >= 0.9){
+                    isPlaying = false
+                    finishBattle()
+                }
+            }
+        }
     }
 
 }
